0904b037989099ae41c57bce1694da3c0f1f9d83
Mia C <miacoupland@btinternet.com>
2025-10-31 22:19:22 +0000

    Add files via upload

--- Jellyfin.Plugin.FinSight/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.FinSight/Configuration/PluginConfiguration.cs
@@ -1,57 +1,30 @@
+namespace Jellyfin.Plugin.Finsight.Configuration;
+
 using MediaBrowser.Model.Plugins;
 
-namespace Jellyfin.Plugin.FinSight.Configuration;
-
-/// <summary>
-/// The configuration options.
-/// </summary>
 public enum SomeOptions
 {
-    /// <summary>
-    /// Option one.
-    /// </summary>
     OneOption,
 
-    /// <summary>
-    /// Second option.
-    /// </summary>
-    AnotherOption
+    AnotherOption,
 }
 
-/// <summary>
-/// Plugin configuration.
-/// </summary>
 public class PluginConfiguration : BasePluginConfiguration
 {
-    /// <summary>
-    /// Initializes a new instance of the <see cref="PluginConfiguration"/> class.
-    /// </summary>
     public PluginConfiguration()
     {
         // set default options here
-        Options = SomeOptions.AnotherOption;
-        TrueFalseSetting = true;
-        AnInteger = 2;
-        AString = "string";
+        this.Options = SomeOptions.AnotherOption;
+        this.TrueFalseSetting = true;
+        this.AnInteger = 2;
+        this.AString = "string";
     }
 
-    /// <summary>
-    /// Gets or sets a value indicating whether some true or false setting is enabled..
-    /// </summary>
     public bool TrueFalseSetting { get; set; }
 
-    /// <summary>
-    /// Gets or sets an integer setting.
-    /// </summary>
     public int AnInteger { get; set; }
 
-    /// <summary>
-    /// Gets or sets a string setting.
-    /// </summary>
     public string AString { get; set; }
 
-    /// <summary>
-    /// Gets or sets an enum option.
-    /// </summary>
     public SomeOptions Options { get; set; }
 }
